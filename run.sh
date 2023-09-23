b62add7cd9fbdcec6be731787af65f42b75988a0
matthew p robertson <35159349+mpr1255@users.noreply.github.com>
2023-09-23 11:10:56 +1000

    updates

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Permissions
 sudo chmod -x /etc/update-motd.d/*
 
 # Update and upgrade
@@ -7,39 +9,32 @@ sudo apt update && sudo apt upgrade -y
 # Install Zsh
 sudo apt install zsh -y
 
-# Install necessary packages for plugins and other functionality
+# Install packages
 sudo apt install git bat autojump python3-pip exa ripgrep zoxide fd-find autojump libfuse2 nnn -y
 
 # Install Neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
-
+if [ ! -f "/usr/local/bin/nvim" ]; then
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    sudo mv nvim.appimage /usr/local/bin/nvim
+fi
 
 # Install LazyGit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+if ! command -v lazygit &> /dev/null; then
+    # Code to install LazyGit
+fi
 
 # Install McFly
-# Check if mcfly is already installed
 if ! command -v mcfly &> /dev/null; then
-	curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly
-    # Install mcfly here
+    curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly
 fi
 
-default_histfile="${HISTFILE:-$HOME/.zsh_history}"
-export MCFLY_HISTFILE="${MCFLY_HISTFILE:-$default_histfile}"
-if [[ ! -r "${MCFLY_HISTFILE}" ]]; then
-  echo "McFly: ${MCFLY_HISTFILE} does not exist or is not readable. Please fix this or set MCFLY_HISTFILE to something else before using McFly."
-  return 1
+# Install Oh My Zsh only if it isn't installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-# install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install zellij
+# Install Zellij
 if [ ! -f "/usr/local/bin/zellij" ]; then
     wget https://github.com/zellij-org/zellij/releases/download/v0.38.2/zellij-x86_64-unknown-linux-musl.tar.gz
     tar -xvf zellij-x86_64-unknown-linux-musl.tar.gz
@@ -47,36 +42,35 @@ if [ ! -f "/usr/local/bin/zellij" ]; then
     sudo mv zellij /usr/local/bin/
 fi
 
-
-# install fzf
-if [ ! -d "~/.fzf" ]; then
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install -y
+# Install fzf
+if [ ! -d "$HOME/.fzf" ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all
 fi
 
-# Make sure the Neovim config directory exists
+# Ensure ~/.config/nvim exists
 mkdir -p ~/.config/nvim
 
-# Symlink init.lua for Neovim and .zshrc for Zsh
+# Symlink Neovim and Zsh configs
 ln -sf ~/dotfiles/init.lua ~/.config/nvim/init.lua
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
-# Reload the shell or print a message
-if [ -z "$ZSH_CUSTOM" ]; then
-  echo "Please restart your shell or run 'exec zsh -l' to apply changes."
-else
-  source ~/.zshrc
-fi
+# Load new shell settings
+source ~/.zshrc
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
-# Make Zsh default shell (moved to the end)
+# Make Zsh the default shell
 chsh -s $(which zsh)
 
+# Link fd (if not linked)
+if [ ! -L "$HOME/.local/bin/fd" ]; then
+    mkdir -p ~/.local/bin
+    ln -s $(which fdfind) ~/.local/bin/fd
+fi
 
 
-
-ln -s $(which fdfind) ~/.local/bin/fd
-
+default_histfile="${HISTFILE:-$HOME/.zsh_history}"
+export MCFLY_HISTFILE="${MCFLY_HISTFILE:-$default_histfile}"
+if [[ ! -r "${MCFLY_HISTFILE}" ]]; then
+  echo "McFly: ${MCFLY_HISTFILE} does not exist or is not readable. Please fix this or set MCFLY_HISTFILE to something else before using McFly."
+  return 1
+fi
