2a9dbe4d93d6fcb09e0972186ea12e0a1673880e
matthew p robertson <35159349+mpr1255@users.noreply.github.com>
2024-12-19 17:43:34 +1100

    minor update

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-rm -rf ./dotfiles && git clone https://github.com/mpr1255/dotfiles.git &&  chmod +x ./dotfiles/run.sh && ./dotfiles/run.sh
+trm -rf ./dotfiles && git clone https://github.com/mpr1255/dotfiles.git &&  chmod +x ./dotfiles/run.sh && ./dotfiles/run.sh
